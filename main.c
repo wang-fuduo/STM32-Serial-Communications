3607897f07f619b1ce4088a3fe842437abeff659
晚风急 <110216399+wang-fuduo@users.noreply.github.com>
2024-10-29 13:23:25 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+extern UART_HandleTypeDef huart1;   //声明串口
+int fputc(int ch, FILE *f)     // 输出重定向
+{
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xffff);
+  return ch;
+}
+int fgetc(FILE *f)
+{
+  uint8_t ch = 0;
+  HAL_UART_Receive(&huart1, &ch, 1, 0xffff);
+  return ch;
+}
